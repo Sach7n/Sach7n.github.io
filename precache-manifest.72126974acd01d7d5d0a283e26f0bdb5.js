cad0fded5fb5684fdff1f12ada11171db351d2e3
Sachin Macwan <47908070+Sach7n@users.noreply.github.com>
2020-12-04 18:48:24 +1100

    Add files via upload

---
+++ precache-manifest.72126974acd01d7d5d0a283e26f0bdb5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e90f95ac419364147a2b7299f0ed95c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ebd9f76d321ea64e75c2",
+    "url": "/static/css/2.d4d1df98.chunk.css"
+  },
+  {
+    "revision": "69ee3461c59cbc801aeb",
+    "url": "/static/css/main.67ab4235.chunk.css"
+  },
+  {
+    "revision": "ebd9f76d321ea64e75c2",
+    "url": "/static/js/2.9e15215b.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.9e15215b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69ee3461c59cbc801aeb",
+    "url": "/static/js/main.9ee784e7.chunk.js"
+  },
+  {
+    "revision": "d6575b1e75c9d19bed0c",
+    "url": "/static/js/runtime-main.5687dd5c.js"
+  },
+  {
+    "revision": "b00bbd616f27a72fa36a9f0be8032780",
+    "url": "/static/media/2.b00bbd61.PNG"
+  },
+  {
+    "revision": "e62dec0cbc677f7d3909078df1f4a848",
+    "url": "/static/media/3.e62dec0c.png"
+  },
+  {
+    "revision": "3e8b104742b6755981202e4d46d023a5",
+    "url": "/static/media/4.3e8b1047.png"
+  },
+  {
+    "revision": "19f37c4adad7a8ed87070f792647f7f1",
+    "url": "/static/media/ResumeF.19f37c4a.pdf"
+  },
+  {
+    "revision": "1721068eafac3dd867e17d7c1844e150",
+    "url": "/static/media/ic2.1721068e.png"
+  }
+]);
